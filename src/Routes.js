938158be55d72d28c2e1155e7da02047427f345e
timothyoh125 <timothy.sun.oh@gmail.com>
2021-03-31 22:42:52 -0700

    Added functionality to create notes and get user's notes

--- src/Routes.js
+++ src/Routes.js
@@ -1,13 +1,29 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
+import Login from "./containers/Login";
+import Signup from "./containers/Signup";
+import NewNote from "./containers/NewNote";
+import Notes from "./containers/Notes";
 import NotFound from "./containers/NotFound";
 
 export default function Routes() {
     return (
         <Switch> 
-            <Route exact path = "/">
+            <Route exact path="/">
                 <Home />
+            </Route>
+            <Route exact path="/login">
+                <Login />
+            </Route>
+            <Route exact path="/signup">
+                <Signup />
+            </Route>
+            <Route exact path="/notes/new">
+                <NewNote />
+            </Route>
+            <Route exact path="/notes/:id">
+                <Notes />
             </Route>
             <Route>
                 <NotFound />
